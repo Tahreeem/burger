f1e58328c7024b4749397ecb7d1659988e9fa890
Tahreem <tahreemsohailbutt@gmail.com>
2019-03-15 19:54:39 -0400

    added redirect for "/" , "" and "/index" to "/burgers"

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -15,17 +15,15 @@ module.exports = function (app) {
   // Each of the below routes just handles the HTML page that the user gets sent to.
 
   app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-    // res.render('index', {    //NOT SURE YET!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    //   layout: 'main'
-    // });
+    res.redirect("/burgers");
+  })
+
+  app.get("", function (req, res) {
+    res.redirect("/burgers");
   })
 
   app.get("/index", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-    // res.render('index', {    //NOT SURE YET!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    //   layout: 'main'
-    // });
+    res.redirect("/burgers");
   });
 
   app.get("/burgers", function (req, res) {
