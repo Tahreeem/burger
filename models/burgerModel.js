98631a7e3fade056b2049e90ab039f6050bb4d01
Tahreem <tahreemsohailbutt@gmail.com>
2019-03-15 20:57:22 -0400

    debugging heroku mySQL connection

--- models/burgerModel.js
+++ models/burgerModel.js
@@ -2,17 +2,17 @@ var orm = require("../config/orm.js");
 
 var burgerModel = {
     create: function (parameters, cb) {
-        orm.insert("burgers_db.burgers", parameters, cb, function (res) {
+        orm.insert("burgers", parameters, cb, function (res) {
             cb(res);
         });
     },
     update: function (set, where, cb) {
-        orm.update("burgers_db.burgers", set, where, function (res) {
+        orm.update("burgers", set, where, function (res) {
             cb(res);
         });
     },
     all: function (cb) {
-        orm.all("burgers_db.burgers", function (res) {
+        orm.all("burgers", function (res) {
             cb(res);
         });
     }
