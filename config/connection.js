f5b3d11329aec244e199c8ccda2b917db7881743
Tahreem <tahreemsohailbutt@gmail.com>
2019-03-16 12:43:56 -0400

    m

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,6 @@ require('dotenv').config();
 //_______________________________________________________________________
 var connection;
 
-<<<<<<< HEAD
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
     console.log((connection));
@@ -18,20 +17,6 @@ else {
         database: "burgers_db"
     });
 }
-=======
-// if (process.env.JAWSDB_URL) {
-connection = mysql.createConnection(process.env.JAWSDB_URL);
-// }
-// else {
-//     connection = mysql.createConnection({
-//         host: process.env.SQL_HOST,
-//         port: process.env.SQL_PORT,
-//         user: process.env.SQL_USER,
-//         password: process.env.SQL_PASS,
-//         database: "burgers_db"
-//     });
-// }
->>>>>>> 8b766133407f43a1cc1794d150796047813c6b21
 
 
 connection.connect();
