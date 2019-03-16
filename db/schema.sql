9ee9f68121df28f00eec62973c82680ce18bf147
Tahreem <tahreemsohailbutt@gmail.com>
2019-03-16 11:40:37 -0400

    changed burger_id to id

--- db/schema.sql
+++ db/schema.sql
@@ -4,10 +4,11 @@ CREATE DATABASE burgers_db;
 
 USE burgers_db;
 
-CREATE TABLE burgers(
-  burger_id INTEGER AUTO_INCREMENT NOT NULL,
-  burger_name VARCHAR(100) NOT NULL,
-  burger_devoured BOOLEAN DEFAULT false,
-  PRIMARY KEY (burger_id),
-  UNIQUE INDEX `burger_name_UNIQUE` (`burger_name` ASC) VISIBLE
+CREATE TABLE `nc5x9ezrcivuo2co`.`burgers` (
+  `burger_id` INTEGER AUTO_INCREMENT NOT NULL,
+  `burger_name` VARCHAR(100) NOT NULL,
+  `burger_devoured` BOOLEAN DEFAULT false,
+  `createdAt` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`burger_id`),
+  UNIQUE INDEX `burger_name_UNIQUE` (`burger_name` ASC)
 );
